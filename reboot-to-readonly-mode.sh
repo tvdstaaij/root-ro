8d6018db9435e3d8bd25951b74fecefe338c6bb5
Tim van der Staaij <git@tim.vanderstaaij.email>
2023-01-08 17:03:39 +0100

    Clean up system journal and bash history when switching to RO

--- reboot-to-readonly-mode.sh
+++ reboot-to-readonly-mode.sh
@@ -1,5 +1,9 @@
 #/bin/sh
 rm /disable-root-ro
+journalctl --rotate
+journalctl --vacuum-time=1s
+rm /root/.bash_history
+rm /home/*/.bash_history
 systemctl stop systemd-random-seed.service
 if [ -f /var/lib/systemd/random-seed ]; then
   rm -f /var/lib/systemd/random-seed
